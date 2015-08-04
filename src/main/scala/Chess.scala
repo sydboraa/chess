9c58ce9d244a455d2ae6d755718b8c6942ff6887
sydboraa <seydabora90@gmail.com>
2015-08-04 16:47:21 +0300

    Added some error controls to main function. Some functions removed from Chess.scala and transported to Utils.scala

--- src/main/scala/Chess.scala
+++ src/main/scala/Chess.scala
@@ -1,6 +1,7 @@
 import model._
 import model.pieces._
 import service.CalculationService
+import utils.Util
 
 import scala.Console._
 
@@ -8,56 +9,39 @@ import scala.Console._
  * Created by SB on 02/08/15.
  */
 object Chess {
+
   def main(args: Array[String]) {
     //board M*N
     println("Please enter the board size M")
     val boardSizeM = in.readLine()
     println("Please enter the board size N")
     val boardSizeN = in.readLine()
-    println("Please enter the pieces of type")
-    val piecesType = in.readLine().trim
-    println(s"The play is starting with ${boardSizeM}*${boardSizeN} and ${piecesType}")
 
-    val board = Board.createEmptyBoard(boardSizeM.toInt, boardSizeN.toInt)
+    if(!Util.boardSizeControl(boardSizeM, boardSizeN)){
+      println("Please enter valid size for board and piece types! (it must be numeric and not empty)")
+    } else {
+      println("Please enter the pieces of type")
+      val piecesType = in.readLine().trim
+      println(s"The play is starting with ${boardSizeM}*${boardSizeN} and ${piecesType}")
+      val board = Board.createEmptyBoard(boardSizeM.toInt, boardSizeN.toInt)
+      val startTimer : Long = System.nanoTime()
+      val solutions : Set[Board] = CalculationService.calculate(board,Util.getOrderedPieces(piecesType))
+      val stopTimer : Long = System.nanoTime()
 
-    val startTimer : Long = System.nanoTime()
-    val solutions : Set[Board] = CalculationService.calculate(board,getOrderedPieces(piecesType))
-    val stopTimer : Long = System.nanoTime()
-
-    solutions.toSeq.zipWithIndex.foreach {
-      case (r, i) => {
-        val ii = i + 1
-        println(s"Solution $ii")
-        println(r.toString)
-        println()
-      }
-    }
-
-    val elapsedTime : Long = (stopTimer - startTimer) / (1000 * 1000)
-    println(s"Total elapsed time: ${elapsedTime} ms")
-    println(s"Total solution: ${solutions.size}")
-
-  }
-
-  def getOrderedPieces(pieces : String) : List[ChessPiece] = {
-    pieces.nonEmpty match {
-      case true => {
-        val listPiecesTypes: List[ChessPiece] = pieces.toUpperCase.toList.map {
-          piece => piece match {
-            case 'Q' => Queen
-            case 'R' => Rook
-            case 'B' => Bishop
-            case 'N' => Knight
-            case 'K' => King
-            case _ => throw new Exception("Wrong piece type!")
-          }
+      solutions.toSeq.zipWithIndex.foreach {
+        case (r, i) => {
+          val ii = i + 1
+          println(s"Solution $ii")
+          println(r.toString)
+          println()
         }
-        listPiecesTypes.sortBy(_.priority)
       }
-      case false => {
-        throw new Exception("Please enter the pieces")
-      }
-    }
 
+      val elapsedTime : Long = (stopTimer - startTimer) / (1000 * 1000)
+      println(s"Total elapsed time: ${elapsedTime} ms")
+      println(s"Total solution: ${solutions.size}")
+    }
   }
+
+
 }
---
+++ src/main/scala/utils/Util.scala
@@ -0,0 +1,47 @@
+package utils
+
+import model.pieces._
+
+/**
+ * Created by SB on 04/08/15.
+ */
+object Util {
+  def getOrderedPieces(pieces : String) : List[ChessPiece] = {
+    pieces.nonEmpty match {
+      case true => {
+        val listPiecesTypes: List[ChessPiece] = pieces.toUpperCase.toList.map {
+          piece => piece match {
+            case 'Q' => Queen
+            case 'R' => Rook
+            case 'B' => Bishop
+            case 'N' => Knight
+            case 'K' => King
+            case _ => throw new Exception("Wrong piece type!")
+          }
+        }
+        listPiecesTypes.sortBy(_.priority)
+      }
+      case false => {
+        throw new Exception("Please enter the pieces!")
+      }
+    }
+  }
+
+  def boardSizeControl(m : String, n: String) : Boolean = {
+    if(m.isEmpty || n.isEmpty) {
+      toIntControl(m) && toIntControl(n)
+    } else {
+      toIntControl(m) && toIntControl(n)
+    }
+  }
+
+  def toIntControl(maybeNumber: String) : Boolean = {
+    try {
+      maybeNumber.toInt.isInstanceOf[Int]
+    } catch {
+      case ex: NumberFormatException => {
+        false
+      }
+    }
+  }
+}
